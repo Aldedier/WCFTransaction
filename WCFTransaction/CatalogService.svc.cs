a126c699dafba6ea388a68174935c1b8289333f5
OSCAR ANDRES CESPEDES DIAZ <oscar.cespedes@correo.policia.gov.co>
2018-07-03 09:45:42 -0500

    Add project files.

---
+++ WCFTransaction/CatalogService.svc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFTransaction
+{
+    [ServiceBehavior(TransactionIsolationLevel = System.Transactions.IsolationLevel.Serializable, 
+        TransactionTimeout = "00:00:45")]
+    public class CatalogService : ICatalogService
+    {
+        readonly CookieCompanyDBEntities db = new CookieCompanyDBEntities();
+        [OperationBehavior(TransactionScopeRequired = true)]
+        public void AddProduct(string name, Uri imageUrl)
+        {
+            db.Product.Add(new Product { Name = name, Image = imageUrl.AbsoluteUri});
+            db.SaveChanges();
+        }
+
+    }
+}
---
+++ WCFTransactionClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace WCFTransactionClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var scope = new TransactionScope(TransactionScopeOption.RequiresNew))
+            using (var catalog = new CatalogServiceClient())
+            {
+                catalog.AddProduct("Producto 15", new Uri("http://miimage.com"));
+                scope.Complete();
+            }
+            Console.WriteLine("Terminado");
+            Console.ReadKey();
+        }
+    }
+}
